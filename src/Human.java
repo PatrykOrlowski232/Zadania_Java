2f002b612f3feaa7ad1cf859adec783654d60ab8
PatrykOrlowski232 <porlowski232@gmail.com>
2023-02-02 19:49:32 +0100

    Zad 3 i 4

--- src/Human.java
+++ src/Human.java
@@ -1,9 +1,26 @@
 public class Human {
     String name;
     int age;
+    double salary;
 
     Animal pet;
 
     Car car;
+
+    double getSalary()
+
+    {
+        return salary;
+    }
+
+    void setSalary(double x)
+    {
+        if(x < 0)
+            System.out.println("Nie można przypisać ujemnej wartości");
+        else {
+            System.out.println("Bla bla bla biurokratyczne pierdoły");
+            salary += x;
+        }
+    }
 
 }
